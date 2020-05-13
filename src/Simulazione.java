f4e5dbe7c57fe0260f38ef633e420253e67b9f1c
pietrocau <pietrocau000@gmail.com>
2020-05-13 22:06:10 +0200

    generale

--- src/Simulazione.java
+++ src/Simulazione.java
@@ -1,3 +1,5 @@
 public class Simulazione {
-    
+    public static void main(String[] args) {
+        
+    }
 }
--- src/Virus.java
+++ src/Virus.java
@@ -1,19 +1,48 @@
 public class Virus {
-    private double infettività;
-    private double presenza_sintomi;
-    private double morte;
-    private int guarigione;
-    private int malato_senza_sintomi;
-    private int malato_con_sintomi;
 
-    public Virus(double infettività, double presenza_sintomi, double morte, int guarigione ){       //ogni virus ha la probabilità che due
-        this.infettività = infettività;                                                             //individui si contagino, la probabilità di
-        this.presenza_sintomi = presenza_sintomi;                                                   //avere sintomi, la prob di morte e i giorni
-        this.morte = morte;                                                                         //di guarigione.
-        this.guarigione = guarigione;
-        this.malato_con_sintomi = guarigione /3;
-        this.malato_senza_sintomi = guarigione/6;
+    private String nome;            //nome del virus/malattia
+    private float infettivita;      //(numero da 0 a 1) la probabilità che un individuo che entra in contatto con il virus venga contagiato
+    private float sintomaticita;    //(numero da 0 a 1) la probabilità che un individuo contagiato con il virus sviluppi sintomi
+    private float letalita;         //(numero da 0 a 1) la probabilità che un individuo che ha sviluppato sintomi muoia del virus
+    private float durata;           //il numero di giorni che intercorrono fra il momento del contagio e quello della guarigione.
+    private float durataIncubazione; //(numero da 0 a 1) la percentuale della durata della malattia che deve trascorrere prima che l'individuo infetto possa diventare contagioso
+    private float durataAsintomatico;     //(numero da 0 a 1) la percentuale della durata della malattia che deve trascorrere prima che l'individuo infetto possa diventare sintomatico
+
+    public Virus(String nome, float infettivita, float sintomaticita, float letalita, float durata, float durataIncubazione, float durataAsintomatico){     //costruttore Virus
+        this.nome = nome;
+        this.infettivita = infettivita;
+        this.sintomaticita = sintomaticita;
+        this.letalita = letalita;
+        this.durata = durata;
+        this.durataIncubazione =  durataIncubazione;
+        this.durataAsintomatico = durataAsintomatico;
     }
 
+    public String getNome() {
+        return nome;
+    }
 
+    public float getInfettivita() {
+        return infettivita;
+    }
+
+    public float getSintomaticita() {
+        return sintomaticita;
+    }
+
+    public float getLetalita() {
+        return letalita;
+    }
+
+    public float getDurata() {
+        return durata;
+    }
+
+    public float getDurataIncubazione() {
+        return durataIncubazione;
+    }
+
+    public float getDurataAsintomatico() {
+        return durataAsintomatico;
+    }
 }
