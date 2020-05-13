09ce9b1072f65b9c472479bc562ccf6231a7742a
ManuelColapinto <manuelcolapinto99@libero.it>
2020-05-13 16:59:43 +0200

    modifica manu 2

--- src/Individuo.java
+++ src/Individuo.java
@@ -10,14 +10,5 @@ public class Individuo {
         this.inmovimento = inmovimento;
     }
 
-    public float spostX(float posX){                                //ho creato uno spostamento random degli individui
-        posX = (float)(Math.random() * 99);                           // l'ho fatto in x e y separati perche non riesco a capire come ritornare
-        return posX;                                                // i due vaalori insieme :(
-    }
-
-    public float spostY(float posY){
-        posY = (float)(Math.random() * 99);                        //crea un numero random da 0 a 100
-        return posY;
-    }
 
 }
--- src/Virus.java
+++ src/Virus.java
@@ -3,13 +3,16 @@ public class Virus {
     private double presenza_sintomi;
     private double morte;
     private int guarigione;
-
+    private int malato_senza_sintomi;
+    private int malato_con_sintomi;
 
     public Virus(double infettività, double presenza_sintomi, double morte, int guarigione ){       //ogni virus ha la probabilità che due
         this.infettività = infettività;                                                             //individui si contagino, la probabilità di
         this.presenza_sintomi = presenza_sintomi;                                                   //avere sintomi, la prob di morte e i giorni
         this.morte = morte;                                                                         //di guarigione.
         this.guarigione = guarigione;
+        this.malato_con_sintomi = guarigione /3;
+        this.malato_senza_sintomi = guarigione/6;
     }
 
 
