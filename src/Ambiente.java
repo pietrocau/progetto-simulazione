cf36fc792c8519b583b94e04e37365ec3ecac778
ManuelColapinto <manuelcolapinto99@libero.it>
2020-05-23 16:57:33 +0200

    modifica metodi manu

--- src/Ambiente.java
+++ src/Ambiente.java
@@ -77,14 +77,30 @@ public class Ambiente {
     }
 
     public float[] getPosRandom(){ //restitusce un'array contente due float, all'indice 0 la coordinata x, all'indice 1 la coordinata y
-        float[] c = {1f};
-        return c;
+        float x;
+        float y;
+        x = (float)(Math.random()*99);
+        y = (float)(Math.random()*99);
+        float[] PosRand = new float[2];
+        x = PosRand[0];
+        y = PosRand[1];
+        return PosRand;
     }
 
     public float[] getDirRandom(){  //restitusce un'array contente due float, all'indice 0 la direzione x, all'indice 1 la direzione y
-        double x = Math.PI*2;
-        float[] c = {1f};
-        return c;
+        float x;
+        float y;
+        double x1;
+        double y1;
+        double p = Math.PI*2;
+        float[] DirRand = new float[2];
+        x = (float)(Math.random()*p);
+        y = (float)(Math.random()*p);
+        x1 = Math.cos(x);
+        y1 = Math.sin(y);
+        x1 = DirRand[0];
+        y1 = DirRand[1];
+        return DirRand;
     }
 
     public boolean spazioLibero(float x, float y){ //restituisce true se non c'è nessun individuo in quel punto dell'ambiente, false altrimenti
